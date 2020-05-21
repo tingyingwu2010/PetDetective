a5b4dffff73dca3461d9d70be28140da60f91764
Vladimir Ionita <ionita.vladimir@gmail.com>
2020-05-21 10:06:42 -0500

    Refactor `map_structure_builder`

--- BruteForceSolution/map_structure_builder.py
+++ BruteForceSolution/map_structure_builder.py
@@ -1,60 +1,81 @@
 def print_welcome_message():
-    print("In order to build the graph, you will have to enter the nodes and the distance between them.")
-    print("You will have to enter only the pets. The `Car` and the houses will be added by default.")
+    print("In order to build the map, you will have to enter the nodes and the distance between them.")
+    print("For nodes, you will have to enter only the pets. The `Car` and the houses will be added by default.")
     print("All the names must be unique and spaces are prohibited.")
     print()
 
 
-def read_graph_info():
-    """ Reads information about the graph and returns the data structures related to it.
+def fetch_map_information():
+    """Fetch information about the map and return the map data representation.
 
-    :return (list of str, list of str, list of str, list of tuple of str): nodes list, pets list, houses list, edges with weight.
+    Map data representation:
+    {
+        nodes (list of str): list of all nodes
+        car (str): name of car node
+        pets (list of str): list of pet nodes
+        houses (list of str): list of house nodes
+        connections (list of tuple of str): list of connections between nodes in the format (node node distance)
+    }
+
+    Returns:
+        dict: map data representation
     """
+    map_data = {
+        'nodes': None,
+        'car': None,
+        'pets': None,
+        'house': None,
+        'connections': None
+    }
+
     pets_input = input("Enter the pets separated by spaces: ")
     pets = pets_input.split(' ')
+    map_data['pets'] = pets
+
     houses = list(map(lambda x: x+'_House', pets))
+    map_data['houses'] = houses
 
     nodes = ['Car']
+    map_data['car'] = 'Car'
+
     nodes.extend(pets)
     nodes.extend(houses)
+    map_data['nodes'] = nodes
 
-    edges = []
+    connections = []
     for i in range(0, len(nodes)):
         for j in range(i + 1, len(nodes)):
             if i == j:
                 continue
             w = input("Enter the distance between the {} and the {}: ".format(nodes[i], nodes[j]))
-            e = (nodes[i], nodes[j], w)
-            edges.append(e)
-    return nodes, pets, houses, edges
+            conn = (nodes[i], nodes[j], w)
+            connections.append(conn)
+    map_data['connections'] = connections
+
+    return map_data
 
 
-def print_structure_instructions():
-    print("***********************************************")
-    print("Graph Structure below. Copy the following data in the `graph.in` file.")
+def print_map_structure(map_data):
+    """Print map structure
 
-
-def print_structure(nodes, pets, houses, edges):
-    """ Prints the graph structure.
-
-    :param list of str nodes: list of nodes
-    :param list of str pets: list of pets
-    :param list of str houses: list of houses
-    :param list of tuple of str edges: list of edges with the weight
+    Args:
+        map_data (dict): map data representation
     """
-    print(" ".join(nodes))
-    print(nodes[0])
-    print(" ".join(pets))
-    print(" ".join(houses))
+    print("***********************************************")
+    print("Map structure below. Copy the following data in the `map.in` file.")
+    print(" ".join(map_data['nodes']))
+    print(map_data['car'])
+    print(" ".join(map_data['pets']))
+    print(" ".join(map_data['houses']))
 
-    edges_repr = []
-    for e in edges:
-        edges_repr.append(" ".join(e))
-    print("\n".join(edges_repr))
+    connections = map_data['connections']
+    connections_repr = []
+    for conn in connections:
+        connections_repr.append(" ".join(conn))
+    print("\n".join(connections_repr))
 
 
 if __name__ == "__main__":
     print_welcome_message()
-    nodes, pets, houses, edges = read_graph_info()
-    print_structure_instructions()
-    print_structure(nodes, pets, houses, edges)
+    data = fetch_map_information()
+    print_map_structure(data)
