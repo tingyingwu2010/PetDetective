3e2d087baddca0d61313dcc09e6ee1eed2b4ce07
Vladimir Ionita <ionita.vladimir@gmail.com>
2020-05-21 09:01:56 -0500

    Add comments to `Node`

--- BruteForceSolution/Node.py
+++ BruteForceSolution/Node.py
@@ -1,12 +1,33 @@
 class Node:
+    """Represent a node on the map
+
+    Attributes:
+        name (str): the name of the node
+        connections (dict of Node:int): dictionary for connections with other nodes and the distance to them
+    """
+
     def __init__(self, name):
         self.name = name
         self.connections = {}
 
     def add_connection_to(self, node, distance):
+        """Add connection to another node.
+
+        Parameters:
+            node (Node): the node to be connected to
+            distance (int): the distance to the node
+        """
         self.connections[node] = distance
 
     def get_distance_to(self, node):
+        """Get distance to node.
+
+        Parameters:
+            node (Node): the node to get the distance to
+
+        Returns:
+            int: distance to the node
+        """
         return self.connections[node]
 
     def __repr__(self):
