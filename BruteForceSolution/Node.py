aac9269cee0ff6076110eaa17d18ec7be42a5ac3
Vladimir Ionita <ionita.vladimir@gmail.com>
2020-05-20 21:27:03 -0500

    Add `get_weight` to the `Node`

--- BruteForceSolution/Node.py
+++ BruteForceSolution/Node.py
@@ -6,5 +6,8 @@ class Node:
     def add_connection(self, node, weight):
         self.connections[node] = weight
 
+    def get_weight(self, node):
+        return self.connections[node]
+
     def __repr__(self):
         return self.name
