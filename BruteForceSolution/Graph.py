17cf669d4d5a18ebe1d0cc4378dbc18f6f234902
Vladimir Ionita <ionita.vladimir@gmail.com>
2020-05-20 21:26:43 -0500

    Create `Graph`

---
+++ BruteForceSolution/Graph.py
@@ -0,0 +1,13 @@
+class Graph:
+    def __init__(self, nodes):
+        self.nodes = nodes
+
+    def get_node(self, name):
+        """ Returns a node by name.
+
+        :param str name: node name
+        :return Node: node
+        """
+        for n in self.nodes:
+            if n.name == name:
+                return n
