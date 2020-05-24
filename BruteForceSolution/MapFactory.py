9c9e69d0875f3fc192cc6c7021b03880b2444375
Vladimir Ionita <ionita.vladimir@gmail.com>
2020-05-24 09:55:53 -0500

    Use the new `create_map_from_node_names` method in `create_map_from_file`

--- BruteForceSolution/MapFactory.py
+++ BruteForceSolution/MapFactory.py
@@ -7,7 +7,7 @@ class MapFactory:
     def create_map_from_file(filepath):
         """Create map from file.
 
-        Args:
+        Parameters:
             filepath (str): file path to load the map from
 
         Returns:
@@ -16,16 +16,10 @@ class MapFactory:
         from utilities import FileUtilities
         file_content = FileUtilities.get_sanitized_content_from_file(filepath)
 
-        all_nodes_name = file_content.pop(0).split(" ")
-        all_nodes = list(map(lambda x: Node(x), all_nodes_name))
-        m = Map(all_nodes)
-
         car_node_name = file_content.pop(0)
-        m.car_node = m.get_node_by_name(car_node_name)
         pet_nodes_name = file_content.pop(0).split(" ")
-        m.pet_nodes = list(map(lambda x: m.get_node_by_name(x), pet_nodes_name))
         house_nodes_names = file_content.pop(0).split(" ")
-        m.house_nodes = list(map(lambda x: m.get_node_by_name(x), house_nodes_names))
+        m = MapFactory.create_map_from_node_names(car_node_name, pet_nodes_name, house_nodes_names)
 
         while file_content:
             conn_repr = file_content.pop(0).split(" ")
