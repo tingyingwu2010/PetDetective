fe5df286995496fa20adb36341d2a9f1d42faaf8
Vladimir Ionita <ionita.vladimir@gmail.com>
2020-05-24 09:55:24 -0500

    Add `create_map_from_node_names` to `MapFactory`

--- BruteForceSolution/MapFactory.py
+++ BruteForceSolution/MapFactory.py
@@ -38,3 +38,27 @@ class MapFactory:
             node_destination.add_connection_to(node_source, distance)
 
         return m
+
+    @staticmethod
+    def create_map_from_node_names(car_node_name, pet_node_names, house_node_names):
+        """Create map from node names.
+
+        Parameters:
+            car_node_name (str): car node's name
+            pet_node_names (list of str): the list of pet's node names
+            house_node_names (list of str): the list of house's node names
+
+        Returns:
+            Map: map
+        """
+        node_names = list(car_node_name)
+        node_names.extend(pet_node_names)
+        node_names.extend(house_node_names)
+        nodes = list(map(lambda x: Node(x), node_names))
+
+        m = Map(nodes)
+        m.car_node = m.get_node_by_name(car_node_name)
+        m.pet_nodes = list(map(lambda x: m.get_node_by_name(x), pet_node_names))
+        m.house_nodes = list(map(lambda x: m.get_node_by_name(x), house_node_names))
+
+        return m
