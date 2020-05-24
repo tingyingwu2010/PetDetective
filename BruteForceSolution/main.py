c7936919932b1e14e23ed80421c0995a911118e1
Vladimir Ionita <ionita.vladimir@gmail.com>
2020-05-23 20:49:32 -0500

    Restructure method `is_route_valid`

--- BruteForceSolution/main.py
+++ BruteForceSolution/main.py
@@ -20,23 +20,25 @@ def get_distance_for_route(route):
     return d
 
 
-def is_route_valid(pet_nodes, house_nodes, route, car_capacity):
-    """ Check if route is valid and return the boolean result.
+def is_route_valid(the_map, route, car_capacity):
+    """Check if route is valid and return the boolean result.
     There are 2 conditions for a route to be valid:
     - no cargo overflow - car capacity is not exceeded
-    - nodes order are consecutive - a pet node is traversed before pet's house node
+    - route nodes order consecutivity is valid - a pet node is traversed before pet's house node
 
-    :param pet_nodes:
-    :param house_nodes:
-    :param route:
-    :param car_capacity:
-    :return:
+    Parameters:
+        the_map (Map): the map that holds the route
+        route (list of Node): a list of nodes representing the route
+        car_capacity (int): the cargo capacity of the car
+
+    Returns:
+        True if the route is valid, False if the route is not valid
     """
     cargo = 0
     for node in route:
-        if node in pet_nodes:
+        if node in the_map.pet_nodes:
             cargo += 1
-        if node in house_nodes:
+        if node in the_map.house_nodes:
             cargo -= 1
         if cargo > car_capacity:    # Cargo Overflow
             return False
@@ -62,7 +64,7 @@ def find_shortest_route_for_capacitated_car(the_map, car_capacity):
     all_distances = {}
     for perm in permutations:
         route = list(perm)
-        if not is_route_valid(the_map.pet_nodes, the_map.house_nodes, route, car_capacity):
+        if not is_route_valid(the_map, route, car_capacity):
             continue
 
         route.insert(0, the_map.car_node)
